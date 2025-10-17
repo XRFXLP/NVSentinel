3da115a3ee0d11783c856fef9a49c70a6f6e92fb
Ajay Mishra <ajmishra@nvidia.com>
2025-10-17 23:57:35 +0530

    chore: remove unwanted commits

--- tilt/simple-health-client/protos/platformconnector.pb.go
+++ tilt/simple-health-client/protos/platformconnector.pb.go
@@ -21,14 +21,13 @@
 package platform_connectors
 
 import (
-	reflect "reflect"
-	sync "sync"
-	unsafe "unsafe"
-
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
+	unsafe "unsafe"
+	sync "sync"
+	unsafe "unsafe"
 )
 
 const (
