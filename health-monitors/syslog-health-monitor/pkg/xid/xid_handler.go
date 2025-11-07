dfbbc87bdac75397704bd035b157d0bf85298bc4
Ajay Mishra <ajmishra@nvidia.com>
2025-11-07 13:37:47 +0530

    fix: unit tests
    Signed-off-by: Ajay Mishra <ajmishra@nvidia.com>

--- health-monitors/syslog-health-monitor/pkg/xid/xid_handler.go
+++ health-monitors/syslog-health-monitor/pkg/xid/xid_handler.go
@@ -113,11 +113,13 @@ func (xidHandler *XIDHandler) determineFatality(recommendedAction pb.Recommended
 }
 
 func (xidHandler *XIDHandler) getGPUUUID(normPCI string) string {
-	if gpuInfo, err := xidHandler.metadataReader.GetGPUByPCI(normPCI); err == nil && gpuInfo != nil {
+	gpuInfo, err := xidHandler.metadataReader.GetGPUByPCI(normPCI)
+	if err == nil && gpuInfo != nil {
 		return gpuInfo.UUID
-	} else if err != nil {
-		slog.Error("Error getting GPU UUID", "pci", normPCI, "error", err)
-		return ""
+	}
+
+	if err != nil {
+		slog.Error("Error getting GPU UUID from metadata", "pci", normPCI, "error", err)
 	}
 
 	if uuid, ok := xidHandler.pciToGPUUUID[normPCI]; ok {
