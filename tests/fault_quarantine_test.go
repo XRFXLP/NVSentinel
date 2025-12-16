5c64208b178949f2eb6160a408ba94f1a1e917fe
Ajay Mishra <ajmishra@nvidia.com>
2025-12-16 10:14:19 +0530

    chore: fix module not used
    Signed-off-by: Ajay Mishra <ajmishra@nvidia.com>

--- tests/fault_quarantine_test.go
+++ tests/fault_quarantine_test.go
@@ -21,13 +21,13 @@ import (
 	"context"
 	"testing"
 
+	"tests/helpers"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/client-go/util/retry"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
-	"tests/helpers"
 )
 
 func TestDontCordonIfEventDoesntMatchCELExpression(t *testing.T) {
