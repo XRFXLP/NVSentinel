614ae452fa50f992f9fad25f5843fdcae04382ec
Ajay Mishra <ajmishra@nvidia.com>
2026-07-09 19:26:19 +0530

    wrap error
    Signed-off-by: Ajay Mishra <ajmishra@nvidia.com>

--- store-client/pkg/client/resume_token.go
+++ store-client/pkg/client/resume_token.go
@@ -151,7 +151,8 @@ func (s *kubernetesResumeControlStore) GetMode(ctx context.Context, clientName s
 	}
 
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to get resume control config map %s in namespace %s: %w",
+			s.name, s.namespace, err)
 	}
 
 	if cm.Data == nil {
@@ -203,7 +204,7 @@ func (s *kubernetesResumeControlStore) SetMode(ctx context.Context, clientName, 
 		cm.Data[clientName] = mode
 
 		_, err = s.client.CoreV1().ConfigMaps(s.namespace).Update(ctx, cm, metav1.UpdateOptions{})
-		
+
 		return err
 	})
 }
